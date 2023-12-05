782a3180a43eec42836badd4fe3cbadf1a5aea55
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-04 21:38:22 -0500

    add selector to the button close

--- cypress/e2e/popup_alerts.cy.js
+++ cypress/e2e/popup_alerts.cy.js
@@ -21,7 +21,10 @@ describe('Lidando com Popups e Alerts', () => {
     const txtEsperado =
       'We can inject and use JavaScript code if all else fails! Remember always try to use WebDriver Library method(s) first such as WebElement.click(). (The Selenium development team have spent allot of time developing WebDriver functions etc).'
     cy.contains('.modal-body', txtEsperado).should('be.visible')
-    cy.get('.modal-content').find('.modal-footer').contains('Close').click()
+    cy.get('.modal-content')
+      .find('.modal-footer')
+      .contains('button', 'Close')
+      .click()
     cy.get('.modal-body').should('not.be.visible')
   })
 
